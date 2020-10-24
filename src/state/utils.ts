d141cc4224e03a7720dfcc6adb8b806ba0f16ce7
alexsocha <alex@alexsocha.com>
2020-10-24 18:08:13 +1100

    fix: check if localStorage is defined

--- src/state/utils.ts
+++ src/state/utils.ts
@@ -16,21 +16,20 @@ export const saveState = (state: RootState): void => {
         pLang: state.pLang,
         code: aceState.editor.getValue(),
     };
-    localStorage.setItem(storageKey, JSON.stringify(savedState));
+
+    if (localStorage) localStorage.setItem(storageKey, JSON.stringify(savedState));
 };
 
 export const loadState = (initState: InitStateFn): RootState => {
-    const savedStateStr = localStorage.getItem(storageKey);
-    if (savedStateStr) {
-        const savedState = JSON.parse(savedStateStr) as SavedState;
-        const pLang = savedState.pLang;
-        const code = savedState.code;
+    if (!(localStorage && localStorage.getItem(storageKey))) return initState(PLang.JS);
 
-        const defaultState = initState(pLang);
-        return {
-            ...defaultState,
-            editorState: { ...defaultState.editorState, savedCode: { [pLang]: code } },
-        };
-    }
-    return initState(PLang.JS);
+    const savedState = JSON.parse(localStorage.getItem(storageKey)!) as SavedState;
+    const pLang = savedState.pLang;
+    const code = savedState.code;
+
+    const defaultState = initState(pLang);
+    return {
+        ...defaultState,
+        editorState: { ...defaultState.editorState, savedCode: { [pLang]: code } },
+    };
 };
